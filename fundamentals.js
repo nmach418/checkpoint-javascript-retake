9254adf44c4f2267f4dd714b6a44092e4d5e1a3f
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-18 13:09:51 -0400

    partial js fund update

--- fundamentals.js
+++ fundamentals.js
@@ -2,27 +2,38 @@
 
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
-
+var colors = ["red", "green", "blue"]
 
 
 // #2: Access the last item in the array and assign to a variable called `lastColor`.
 // Type your solution immediately below this line:
+
+var lastColor = colors[2]
 
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
 
+var favoriteColors = []
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
 
+for (var i = 0; i < colors; i++)
+    favoriteColors.push(colors[i]);
+
 
 
 // #5: Create an object literal called `student` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+ 
+var student = {
+    firstName: "George",
+    lastName: "Washington",
+    age: 50
+}
 
 
 // #6: Add a `attends-office-hours` (spelled exactly) property to `student` by accessing
